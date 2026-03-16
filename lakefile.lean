0f0445f7058e602234498ffb7c5c1c9c1c17e4a6
François Garillot <francois@garillot.net>
2026-03-16 07:14:33 -0400

    Update Lean 4 and Mathlib to v4.24.0
    Upgrade toolchain and dependencies:
    - Lean 4: v4.14.0 -> v4.24.0
    - Mathlib: corresponding update

--- lakefile.lean
+++ lakefile.lean
@@ -5,7 +5,7 @@ open Lake DSL
 package LeanLearn
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.24.0"
 
 @[default_target]
 lean_lib Succinct
